14f29c5a91357024cebeff256dd17ec74cbb36bd
leandroGenios <leandro.genios@gmail.com>
2019-03-06 07:42:34 -0300

    .

--- MaisPratosAndroid/app/src/main/java/com/tcc/maispratos/activity/Login.java
+++ MaisPratosAndroid/app/src/main/java/com/tcc/maispratos/activity/Login.java
@@ -1,7 +1,10 @@
 package com.tcc.maispratos.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.tcc.maispratos.R;
 
@@ -12,5 +15,14 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+
+        Button cadastrar = (Button) findViewById(R.id.btnCadastrar);
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CadastroUsuario.class);
+                startActivity(intent);
+            }
+        });
     }
 }

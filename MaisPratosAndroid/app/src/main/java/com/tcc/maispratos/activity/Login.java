205e2440b39bb837fcf06473c6f81bd2c55f4b6a
Leandro Soares <leandro.moreira@dbmaster.com.br>
2019-05-02 17:58:58 -0300

    .

--- MaisPratosAndroid/app/src/main/java/com/tcc/maispratos/activity/Login.java
+++ MaisPratosAndroid/app/src/main/java/com/tcc/maispratos/activity/Login.java
@@ -35,7 +35,7 @@ public class Login extends AppCompatActivity {
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CadastroIngrediente.class);
+                Intent intent = new Intent(getApplicationContext(), PratosActivity.class);
                 startActivity(intent);
             }
         });
